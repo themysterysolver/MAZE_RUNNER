e404acc6a17f5909263cbe970c4cca2f77b12b88
themysterysolver <arjunprabhakar1910@gmail.com>
2025-05-31 09:33:59 +0530

    FEAT:wall logic+game logic+win check

--- script.js
+++ script.js
@@ -70,14 +70,54 @@ class Tile{
     red.classList.add("redIt");
    tileArray[start1].ele.appendChild(red);
 
-   //EVENT-LOOP
-   document.addEventListener('keydown',function(e){
-    switch(e.key){
-        case 'ArrowUp': console.log("UP"); break;
-        case 'ArrowDown': console.log("DOWN");break;
-        case 'ArrowLeft': console.log("LEFT");break;
-        case 'ArrowRight': console.log("RIGHT");break;
-      }
-    });
+   
 
 tileArray[22].ele.innerHTML="<div class=\"endTile\">END</div>";
+
+hasWon=(current)=>{
+    if(current==end1){
+        alert("You won!");
+        this.location.reload();
+        return true;
+    }
+    return false;
+}
+//EVENT-LOOP
+let current=start1;
+document.addEventListener('keydown',function(e){
+    const [L,R,T,B]=tileArray[current].dir;
+    switch(e.key){
+        case 'ArrowUp': 
+            if(current-sqSize>=0 && !T){
+                red.remove();
+                current-=sqSize;
+                if(hasWon(current)){return};
+                tileArray[current].ele.appendChild(red);
+            }
+            break;
+        case 'ArrowDown': 
+            if(current+sqSize<sqSize*sqSize && !B){
+                red.remove();
+                current+=sqSize;
+                if(hasWon(current)){return};
+                tileArray[current].ele.appendChild(red);
+            }
+            break;
+        case 'ArrowLeft':
+            if(current-1>=0 && !L){
+                red.remove();
+                current-=1
+                if(hasWon(current)){return};
+                tileArray[current].ele.appendChild(red);
+            } 
+            break;
+        case 'ArrowRight':
+            if(current+1<sqSize*sqSize && !R){
+                red.remove();
+                current+=1
+                if(hasWon(current)){return};
+                tileArray[current].ele.appendChild(red);
+            }
+            break;
+        }
+});
