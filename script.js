23e5c81b203eb17245d77b1ca6fa36b0882b4573
themysterysolver <arjunprabhakar1910@gmail.com>
2025-05-31 09:38:17 +0530

    CHORE:PRETTY ALIGN

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
 class Tile{
-            constructor(id,arr,i,j){
-            this.ID=id;
-            this.dir=arr; // L R T B
-            this.ele=document.createElement('div');
-            this.ele.classList.add("tile");
-            this.ele.id=this.ID
-            const [L,R,T,B]=this.dir;
-            this.x=i;
-            this.y=j;
-            this.ele.style.width=tileSize+"px";
-            this.ele.style.height=tileSize+"px";
+        constructor(id,arr,i,j){
+        this.ID=id;
+        this.dir=arr; // L R T B
+         this.ele=document.createElement('div');
+        this.ele.classList.add("tile");
+        this.ele.id=this.ID
+        const [L,R,T,B]=this.dir;
+        this.x=i;
+        this.y=j;
+        this.ele.style.width=tileSize+"px";
+       this.ele.style.height=tileSize+"px";
 
-            this.ele.style.borderLeft= L ? "2px solid black": "none";
-            this.ele.style.borderRight= R ? "2px solid black": "none";
-            this.ele.style.borderTop= T ? "2px solid black": "none";
-            this.ele.style.borderBottom= B ? "2px solid black": "none";
+        this.ele.style.borderLeft= L ? "2px solid black": "none";
+        this.ele.style.borderRight= R ? "2px solid black": "none";
+        this.ele.style.borderTop= T ? "2px solid black": "none";
+        this.ele.style.borderBottom= B ? "2px solid black": "none";
+    }
+    getElement(){
+        return this.ele;
+    }
+}
+console.log(mazeMap);
+
+const sqSize=Math.sqrt(Math.max(...Object.keys(mazeMap).map(Number))+1);
+console.log(sqSize);
+
+const sq=document.getElementById("square");
+console.log(sq);
+const tileArray=[];
+
+const tileSize=50;
 
 
-        }
-        getElement(){
-            return this.ele;
+for(let i=0;i<sqSize;i++){
+    for(let j=0;j<sqSize;j++){
+        let t=new Tile(sqSize*i+j,mazeMap[sqSize*i+j],i,j)
+        sq.appendChild(t.getElement())
+        tileArray.push(t);
+    }
+}
+console.log(sq);
+
+
+sq.style.width=(sqSize*tileSize)+"px";
+sq.style.height=(sqSize*tileSize)+"px";
+
+let dir=[[0,-1],[-1,0]]
+tileArray.forEach(element => {
+    let x=element.x;
+    let y=element.y;
+    const [L,R,T,B]=element.dir;
+    if(x>0){
+        if(T){
+            element.ele.style.borderTop="none";
         }
     }
-    console.log(mazeMap);
-
-    const sqSize=Math.sqrt(Math.max(...Object.keys(mazeMap).map(Number))+1);
-    console.log(sqSize);
-
-    const sq=document.getElementById("square");
-    console.log(sq);
-    const tileArray=[];
-
-    const tileSize=50;
-
-
-    for(let i=0;i<sqSize;i++){
-        for(let j=0;j<sqSize;j++){
-            let t=new Tile(sqSize*i+j,mazeMap[sqSize*i+j],i,j)
-            sq.appendChild(t.getElement())
-            tileArray.push(t);
+    if(y>0){
+        if(L){
+            element.ele.style.borderLeft="none";
         }
     }
-    console.log(sq);
+});
 
 
-    sq.style.width=(sqSize*tileSize)+"px";
-    sq.style.height=(sqSize*tileSize)+"px";
-
-    let dir=[[0,-1],[-1,0]]
-    tileArray.forEach(element => {
-        let x=element.x;
-        let y=element.y;
-        const [L,R,T,B]=element.dir;
-        if(x>0){
-            if(T){
-                element.ele.style.borderTop="none";
-            }
-        }
-        if(y>0){
-            if(L){
-                element.ele.style.borderLeft="none";
-            }
-        }
-    });
-
-
-    //-----------Creating RED------------
-    const red=document.createElement('div');
-    red.classList.add("redIt");
-   tileArray[start1].ele.appendChild(red);
+//-----------Creating RED------------
+const red=document.createElement('div');
+red.classList.add("redIt");
+tileArray[start1].ele.appendChild(red);
 
    
 
